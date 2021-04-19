53f5514594c246c2a5ce05fd83aad4c2fef11a97
isaacwrk <isaacsouzawrk@gmail.com>
2021-04-19 16:44:29 -0300

    inserindo novas pastas e functions

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-function onLoad(){
-    const dependencias = {
-        tela: Tela
-    }
-    const jogoDaMemoria = new JogoDaMemoria(dependencias)
-    jogoDaMemoria.inicializar()
+function onLoad () {
+     
+   const dependencias = {
+       tela: Tela,
+       util: Util
+   }
+
+   const jogoDaMemoria = new JogoDaMemoria(dependencias)
+   jogoDaMemoria.inicializar()
 }
+
 window.onload = onLoad
